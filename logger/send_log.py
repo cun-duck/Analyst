be089ea1c7be28be3e0e37624267381b4b5317b0
Nurul Fauzi <cunduck99@gmail.com>
2024-11-08 22:50:44 +0700

    Update send_log.py

--- logger/send_log.py
+++ logger/send_log.py
@@ -6,8 +6,8 @@ def send_log_to_telegram(file_name):
     """
     Kirim log pengunggahan file ke Telegram
     """
-    token = 'YOUR_BOT_TOKEN'
-    chat_id = 'YOUR_CHAT_ID'
+    token = '7590725563:AAH0AoLSrT5WatVESyDYPKkgW53UtMWp4XI'
+    chat_id = '5217803272'
     message = f'File uploaded: {file_name}'
     url = f'https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id}&text={message}'
     requests.get(url)
